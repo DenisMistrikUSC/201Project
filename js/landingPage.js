ce62835c3e892daf309ca59ce9e5df4928d555a0
kojo boakye <boakyeni@usc.edu>
2021-11-18 15:35:49 -0800

    sign in script complete, only needs name of json file

--- js/landingPage.js
+++ js/landingPage.js
@@ -2,14 +2,28 @@ function helloWord() {
     window.alert("Hello Word")
 
 }
-
+/*
+This function retrives user input and validates with the server
+*/
 function signIn() {
     event.preventDefault();
-    var userName = document.getElementById("username-field");
-    var passWord = document.getElementById("password-field");
-    console.log("in signInfunction ");
-    console.log(userName.value, passWord.value);
+    var userName = document.getElementById("username-field").value;
+    var passWord = document.getElementById("password-field").value;
+    /*
+    Post should request the JSON file containing the User and Icon
+    to verify to user has an account
+    */
+    $.post("---",
+    {
+      //user name data to pass along to server
+      user: userName.toLowerCase(),
+    },
+    //if the post succeds redirect to either drawing page or watching page
+    function(data){
+      window.location.href = data;
+    });
 }
+
 
 function signUp() {
     event.preventDefault();
