55e794f22a7264b555f347820b144cea56d4ac5e
Abdulah Khan <amkhan@usc.edu>
2021-12-03 17:42:04 -0800

    changes to validation files

--- src/main/java/ValidateLogin.java
+++ src/main/java/ValidateLogin.java
@@ -7,15 +7,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class ValidateLogin 
+@WebServlet("/loginPage")
+public class ValidateLogin extends HttpServlet
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String name;  
 	String pass; 
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 	{
 		response.setContentType("text/html;charset=UTF-8");
--- src/main/java/ValidateSignUp.java
+++ src/main/java/ValidateSignUp.java
@@ -6,12 +6,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ValidateSignUp 
+@WebServlet("/SignupPage")
+public class ValidateSignUp extends HttpServlet
 {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 	{
 		response.setContentType("text/html;charset=UTF-8");
